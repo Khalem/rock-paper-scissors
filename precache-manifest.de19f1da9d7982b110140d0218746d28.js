e0171507dcd7d61f761b730303e93c78eee82fb7
Khalem <33363455+Khalem@users.noreply.github.com>
2021-02-17 18:40:40 +0000

    Updates

---
+++ precache-manifest.de19f1da9d7982b110140d0218746d28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7b4515e183a13341bf71413e02014a",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "1941190e04b237fb89c9",
+    "url": "/rock-paper-scissors/static/css/main.3db7d705.chunk.css"
+  },
+  {
+    "revision": "629c420ceb65a367a40f",
+    "url": "/rock-paper-scissors/static/js/2.81fce73a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.81fce73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1941190e04b237fb89c9",
+    "url": "/rock-paper-scissors/static/js/main.2cbbe2d6.chunk.js"
+  },
+  {
+    "revision": "4152367e75813356d958",
+    "url": "/rock-paper-scissors/static/js/runtime-main.9b8d27fd.js"
+  }
+]);
